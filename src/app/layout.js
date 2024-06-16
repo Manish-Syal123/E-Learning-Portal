f714ed7486793531076133f7f6c70db3662b4b17
Manish_Syal <manishsyal77@gmail.com>
2024-06-16 19:49:57 +0530

    Created dark/light theme functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Nav from "@/components/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          // defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {/* <Nav /> */}
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
