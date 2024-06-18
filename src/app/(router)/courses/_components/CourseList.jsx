b03498d7d0979e9d2da8916798238796424201d0
Manish_Syal <manishsyal77@gmail.com>
2024-06-18 19:50:20 +0530

    Added Skelton Effect and no-course-found message on condition

--- src/app/(router)/courses/_components/CourseList.jsx
+++ src/app/(router)/courses/_components/CourseList.jsx
@@ -8,18 +8,22 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import CourseItem from "./CourseItem";
+import { Rabbit } from "lucide-react";
 
 const CourseList = () => {
   const [courseList, setCourseList] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     getallCourses();
   }, []);
 
   // Fetch Course List
   const getallCourses = async () => {
+    setLoading(true);
     const res = await getAllCourses();
-    // console.log(res);
-    setCourseList(res?.courseLists);
+    setCourseList(res?.courseLists || []); // Handling case where res.courseLists might be undefined
+    setLoading(false);
   };
 
   return (
@@ -32,20 +36,41 @@ const CourseList = () => {
             <SelectValue placeholder="Filter" />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="light">All</SelectItem>
-            <SelectItem value="dark">Paid</SelectItem>
-            <SelectItem value="system">Free</SelectItem>
+            <SelectItem value="all">All</SelectItem>
+            <SelectItem value="paid">Paid</SelectItem>
+            <SelectItem value="free">Free</SelectItem>
           </SelectContent>
         </Select>
       </div>
 
       {/* Display Course List */}
       <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
-        {courseList.map((item, index) => (
-          <div key={index}>
-            <CourseItem course={item} />
+        {loading ? (
+          [1, 2, 3, 4, 5, 6, 7].map((item, index) => (
+            <div
+              key={index}
+              className="w-full h-[240px] rounded-xl m-2 bg-slate-200 animate-pulse flex flex-col p-3"
+            >
+              <div className="flex-1 bg-slate-400 rounded-xl"></div>
+              <div className="p-4">
+                <div className="h-4 bg-slate-400 rounded-lg mb-2"></div>
+                <div className="h-4 bg-slate-400 rounded-lg mb-2 w-36"></div>
+                <div className="h-4 bg-slate-400 rounded-lg w-24"></div>
+              </div>
+            </div>
+          ))
+        ) : courseList.length > 0 ? (
+          courseList.map((item, index) => (
+            <div key={index}>
+              <CourseItem course={item} />
+            </div>
+          ))
+        ) : (
+          <div className="flex flex-col items-center justify-center mt-10 lg:ml-80 w-full h-full">
+            <Rabbit size={150} />
+            <h2 className="font-bold text-xl">No Courses Found!</h2>
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
