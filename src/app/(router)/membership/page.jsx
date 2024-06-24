586576ffe0ba9ff19e36a43f0c74ff218284762e
Manish_Syal <manishsyal77@gmail.com>
2024-06-24 11:19:46 +0530

    Designed UI for Membership page

---
+++ src/app/(router)/membership/page.jsx
@@ -0,0 +1,265 @@
+import React from "react";
+
+const Membership = () => {
+  return (
+    <div className="mx-auto max-w-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:items-center md:gap-8 mt-6">
+        <div className="rounded-2xl bg-white border border-gray-200 hover:border-indigo-600 p-6 shadow-lg hover:shadow-2xl hover:ring-1 hover:ring-indigo-600 cursor-pointer sm:order-last sm:px-8 lg:p-12">
+          <div className="text-center">
+            <h2 className="text-lg font-medium text-gray-900">
+              Yeraly
+              <span className="sr-only">Plan</span>
+            </h2>
+
+            <p className="mt-2 sm:mt-4">
+              <strong className="text-3xl font-bold text-gray-900 sm:text-4xl">
+                {" "}
+                39.99${" "}
+              </strong>
+
+              <span className="text-sm font-medium text-gray-700">/yera</span>
+            </p>
+          </div>
+
+          <ul className="mt-6 space-y-2">
+            <li className="flex items-center gap-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-indigo-700"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 12.75l6 6 9-13.5"
+                />
+              </svg>
+
+              <span className="text-gray-700"> Access to All Courses </span>
+            </li>
+
+            <li className="flex items-center gap-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-indigo-700"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 12.75l6 6 9-13.5"
+                />
+              </svg>
+
+              <span className="text-gray-700"> Free Source Code</span>
+            </li>
+
+            <li className="flex items-center gap-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-indigo-700"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 12.75l6 6 9-13.5"
+                />
+              </svg>
+
+              <span className="text-gray-700"> Free App Membership </span>
+            </li>
+
+            <li className="flex items-center gap-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-indigo-700"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 12.75l6 6 9-13.5"
+                />
+              </svg>
+
+              <span className="text-gray-700">
+                {" "}
+                Email and Instagram Support
+              </span>
+            </li>
+
+            {/* <li className="flex items-center gap-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-indigo-700"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 12.75l6 6 9-13.5"
+                />
+              </svg>
+
+              <span className="text-gray-700"> Phone support </span>
+            </li>
+
+            <li className="flex items-center gap-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-indigo-700"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 12.75l6 6 9-13.5"
+                />
+              </svg>
+
+              <span className="text-gray-700"> Community access </span>
+            </li> */}
+          </ul>
+
+          <a
+            href="#"
+            className="mt-8 block rounded-full border border-indigo-600 bg-indigo-600 px-12 py-3 text-center text-sm font-medium text-white hover:bg-indigo-700 hover:ring-1 hover:ring-indigo-700 focus:outline-none focus:ring active:text-indigo-500"
+          >
+            Get Started
+          </a>
+        </div>
+
+        <div className="rounded-2xl shadow-lg bg-white border border-gray-200  hover:border-indigo-600 hover:ring-indigo-600 hover:ring-1 hover:shadow-2xl cursor-pointer p-6 sm:px-8 lg:p-12">
+          <div className="text-center">
+            <h2 className="text-lg font-medium text-gray-900">
+              Monthly
+              <span className="sr-only">Plan</span>
+            </h2>
+
+            <p className="mt-2 sm:mt-4">
+              <strong className="text-3xl font-bold text-gray-900 sm:text-4xl">
+                {" "}
+                4.99${" "}
+              </strong>
+
+              <span className="text-sm font-medium text-gray-700">/month</span>
+            </p>
+          </div>
+
+          <ul className="mt-6 space-y-2">
+            <li className="flex items-center gap-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-indigo-700"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 12.75l6 6 9-13.5"
+                />
+              </svg>
+
+              <span className="text-gray-700"> Access to All Courses </span>
+            </li>
+
+            <li className="flex items-center gap-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-indigo-700"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 12.75l6 6 9-13.5"
+                />
+              </svg>
+
+              <span className="text-gray-700"> Free Source Code</span>
+            </li>
+
+            <li className="flex items-center gap-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-indigo-700"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 12.75l6 6 9-13.5"
+                />
+              </svg>
+
+              <span className="text-gray-700"> Free App Membership </span>
+            </li>
+
+            <li className="flex items-center gap-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-indigo-700"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 12.75l6 6 9-13.5"
+                />
+              </svg>
+
+              <span className="text-gray-700">
+                {" "}
+                Email and Instagram DM Suppport{" "}
+              </span>
+            </li>
+          </ul>
+
+          <a
+            href="#"
+            className="mt-8 block rounded-full border border-indigo-600 bg-indigo-600 px-12 py-3 text-center text-sm font-medium text-white hover:bg-indigo-700 hover:ring-1 hover:ring-indigo-700 focus:outline-none focus:ring active:text-indigo-500"
+          >
+            Get Started
+          </a>
+        </div>
+      </div>
+      <h2 className="flex items-center justify-center mt-10 font-bold text-[15px]">
+        Get your first month half price when you use the code MANISHSYAL
+        (monthly plan only).
+      </h2>
+    </div>
+  );
+};
+
+export default Membership;
