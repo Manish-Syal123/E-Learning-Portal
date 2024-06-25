37dc1c42217acebd3f9a35863f706c59e2d7bc72
Manish_Syal <manishsyal77@gmail.com>
2024-06-25 18:15:52 +0530

    Integrated Payment Gateway

--- src/app/(router)/membership/page.jsx
+++ src/app/(router)/membership/page.jsx
@@ -1,107 +1,154 @@
-import React from "react";
+"use client";
+import axios from "axios";
+import Script from "next/script";
+import React, { useState } from "react";
 
 const Membership = () => {
+  const [subscriptionId, setSubscriptionId] = useState(null);
+
+  // to create Subscription Id
+  const createSubscription = async (planId) => {
+    await axios
+      .post(
+        "/api/create-subscription",
+        JSON.stringify({
+          plan_id: planId,
+        })
+      )
+      .then((resp) => {
+        console.log(resp);
+        setSubscriptionId(resp.data.id);
+        makePayment();
+      })
+      .catch((err) => {
+        console.log("Error occured while creating subscription ID: ", err);
+      });
+  };
+
+  const makePayment = () => {
+    const options = {
+      key: process.env.NEXT_PUBLIC_RAZORPAY_LIVE_KEY,
+      subscription_id: subscriptionId,
+      name: "Manish EdTech Academy",
+      description: "ManishEdTech Membership",
+      handler: async (resp) => {
+        console.log(resp);
+      },
+      theme: {
+        color: "#7D41E1",
+      },
+    };
+
+    const rzp = new window.Razorpay(options);
+    rzp.open();
+  };
   return (
-    <div className="mx-auto max-w-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:items-center md:gap-8 mt-6">
-        <div className="rounded-2xl bg-white border border-gray-200 hover:border-indigo-600 p-6 shadow-lg hover:shadow-2xl hover:ring-1 hover:ring-indigo-600 cursor-pointer sm:order-last sm:px-8 lg:p-12">
-          <div className="text-center">
-            <h2 className="text-lg font-medium text-gray-900">
-              Yeraly
-              <span className="sr-only">Plan</span>
-            </h2>
+    <>
+      <Script
+        id="razorpay-checkout-js"
+        src="https:checkout.razorpay.com/v1/checkout.js"
+      ></Script>
+      <div className="mx-auto max-w-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:items-center md:gap-8 mt-6">
+          {/* Yearly Plan */}
+          <div className="rounded-2xl bg-white border border-gray-200 hover:border-indigo-600 p-6 shadow-lg hover:shadow-2xl hover:ring-1 hover:ring-indigo-600 cursor-pointer sm:order-last sm:px-8 lg:p-12">
+            <div className="text-center">
+              <h2 className="text-lg font-medium text-gray-900">
+                Yeraly
+                <span className="sr-only">Plan</span>
+              </h2>
 
-            <p className="mt-2 sm:mt-4">
-              <strong className="text-3xl font-bold text-gray-900 sm:text-4xl">
-                {" "}
-                39.99${" "}
-              </strong>
+              <p className="mt-2 sm:mt-4">
+                <strong className="text-3xl font-bold text-gray-900 sm:text-4xl">
+                  {" "}
+                  39.99${" "}
+                </strong>
 
-              <span className="text-sm font-medium text-gray-700">/yera</span>
-            </p>
-          </div>
+                <span className="text-sm font-medium text-gray-700">/yera</span>
+              </p>
+            </div>
 
-          <ul className="mt-6 space-y-2">
-            <li className="flex items-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-5 text-indigo-700"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
+            <ul className="mt-6 space-y-2">
+              <li className="flex items-center gap-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-indigo-700"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
 
-              <span className="text-gray-700"> Access to All Courses </span>
-            </li>
+                <span className="text-gray-700"> Access to All Courses </span>
+              </li>
 
-            <li className="flex items-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-5 text-indigo-700"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
+              <li className="flex items-center gap-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-indigo-700"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
 
-              <span className="text-gray-700"> Free Source Code</span>
-            </li>
+                <span className="text-gray-700"> Free Source Code</span>
+              </li>
 
-            <li className="flex items-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-5 text-indigo-700"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
+              <li className="flex items-center gap-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-indigo-700"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
 
-              <span className="text-gray-700"> Free App Membership </span>
-            </li>
+                <span className="text-gray-700"> Free App Membership </span>
+              </li>
 
-            <li className="flex items-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-5 text-indigo-700"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
+              <li className="flex items-center gap-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-indigo-700"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
 
-              <span className="text-gray-700">
-                {" "}
-                Email and Instagram Support
-              </span>
-            </li>
+                <span className="text-gray-700">
+                  {" "}
+                  Email and Instagram Support
+                </span>
+              </li>
 
-            {/* <li className="flex items-center gap-1">
+              {/* <li className="flex items-center gap-1">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -138,128 +185,144 @@ const Membership = () => {
 
               <span className="text-gray-700"> Community access </span>
             </li> */}
-          </ul>
+            </ul>
 
-          <a
-            href="#"
-            className="mt-8 block rounded-full border border-indigo-600 bg-indigo-600 px-12 py-3 text-center text-sm font-medium text-white hover:bg-indigo-700 hover:ring-1 hover:ring-indigo-700 focus:outline-none focus:ring active:text-indigo-500"
-          >
-            Get Started
-          </a>
-        </div>
-
-        <div className="rounded-2xl shadow-lg bg-white border border-gray-200  hover:border-indigo-600 hover:ring-indigo-600 hover:ring-1 hover:shadow-2xl cursor-pointer p-6 sm:px-8 lg:p-12">
-          <div className="text-center">
-            <h2 className="text-lg font-medium text-gray-900">
-              Monthly
-              <span className="sr-only">Plan</span>
-            </h2>
-
-            <p className="mt-2 sm:mt-4">
-              <strong className="text-3xl font-bold text-gray-900 sm:text-4xl">
-                {" "}
-                4.99${" "}
-              </strong>
-
-              <span className="text-sm font-medium text-gray-700">/month</span>
-            </p>
+            <a
+              onClick={() =>
+                createSubscription(
+                  process.env.NEXT_PUBLIC_RAZORPAY_YEARLY_PLAN_KEY
+                )
+              }
+              className="mt-8 block rounded-full border border-indigo-600 bg-indigo-600 px-12 py-3 text-center text-sm font-medium text-white hover:bg-indigo-700 hover:ring-1 hover:ring-indigo-700 focus:outline-none focus:ring active:text-indigo-500"
+            >
+              Get Started
+            </a>
           </div>
 
-          <ul className="mt-6 space-y-2">
-            <li className="flex items-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-5 text-indigo-700"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
+          {/* Monthly Plan */}
+          <div className="rounded-2xl shadow-lg bg-white border border-gray-200  hover:border-indigo-600 hover:ring-indigo-600 hover:ring-1 hover:shadow-2xl cursor-pointer p-6 sm:px-8 lg:p-12">
+            <div className="text-center">
+              <h2 className="text-lg font-medium text-gray-900">
+                Monthly
+                <span className="sr-only">Plan</span>
+              </h2>
 
-              <span className="text-gray-700"> Access to All Courses </span>
-            </li>
+              <p className="mt-2 sm:mt-4">
+                <strong className="text-3xl font-bold text-gray-900 sm:text-4xl">
+                  {" "}
+                  4.99${" "}
+                </strong>
 
-            <li className="flex items-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-5 text-indigo-700"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
+                <span className="text-sm font-medium text-gray-700">
+                  /month
+                </span>
+              </p>
+            </div>
 
-              <span className="text-gray-700"> Free Source Code</span>
-            </li>
+            <ul className="mt-6 space-y-2">
+              <li className="flex items-center gap-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-indigo-700"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
 
-            <li className="flex items-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-5 text-indigo-700"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
+                <span className="text-gray-700"> Access to All Courses </span>
+              </li>
 
-              <span className="text-gray-700"> Free App Membership </span>
-            </li>
+              <li className="flex items-center gap-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-indigo-700"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
 
-            <li className="flex items-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-5 text-indigo-700"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
+                <span className="text-gray-700"> Free Source Code</span>
+              </li>
 
-              <span className="text-gray-700">
-                {" "}
-                Email and Instagram DM Suppport{" "}
-              </span>
-            </li>
-          </ul>
+              <li className="flex items-center gap-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-indigo-700"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
 
-          <a
-            href="#"
-            className="mt-8 block rounded-full border border-indigo-600 bg-indigo-600 px-12 py-3 text-center text-sm font-medium text-white hover:bg-indigo-700 hover:ring-1 hover:ring-indigo-700 focus:outline-none focus:ring active:text-indigo-500"
-          >
-            Get Started
-          </a>
+                <span className="text-gray-700"> Free App Membership </span>
+              </li>
+
+              <li className="flex items-center gap-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-indigo-700"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
+
+                <span className="text-gray-700">
+                  {" "}
+                  Email and Instagram DM Suppport{" "}
+                </span>
+              </li>
+            </ul>
+
+            <a
+              // href="#"
+              onClick={() =>
+                createSubscription(
+                  process.env.NEXT_PUBLIC_RAZORPAY_MONTHLY_PLAN_KEY
+                )
+              }
+              className="mt-8 block rounded-full border border-indigo-600 bg-indigo-600 px-12 py-3 text-center text-sm font-medium text-white hover:bg-indigo-700 hover:ring-1 hover:ring-indigo-700 focus:outline-none focus:ring active:text-indigo-500"
+            >
+              Get Started
+            </a>
+          </div>
         </div>
+        <h2 className="flex items-center justify-center mt-10 font-bold text-[15px]">
+          Get your first month half price when you use the code MANISHSYAL
+          (monthly plan only).
+        </h2>
       </div>
-      <h2 className="flex items-center justify-center mt-10 font-bold text-[15px]">
-        Get your first month half price when you use the code MANISHSYAL
-        (monthly plan only).
-      </h2>
-    </div>
+    </>
   );
 };
 
 export default Membership;
+
+// Test card provided by razorpay while testing/devloping the app // Test Mode
+// https://razorpay.com/docs/payments/payments/test-card-details
